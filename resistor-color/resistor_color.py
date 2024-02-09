084582290a94f7e7f3394e7fc0961439c6499c29
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2024-02-09 08:01:18 +0200

    upload resistor-color excercise

---
+++ resistor-color/resistor_color.py
@@ -0,0 +1,20 @@
+COLORS = {
+    "black": 0,
+    "brown": 1,
+    "red": 2,
+    "orange": 3,
+    "yellow": 4,
+    "green": 5,
+    "blue": 6,
+    "violet": 7,
+    "grey": 8,
+    "white": 9,
+}
+
+
+def color_code(color):
+    return COLORS.get(color.lower())
+
+
+def colors():
+    return list(COLORS)
